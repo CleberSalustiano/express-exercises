5cd59aa177d2b34c0bfc72da91bfc0320b88873f
CleberSalustiano <kleberdejesus45@hotmail.com>
2022-06-09 22:26:40 -0300

    feat: License and README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# Express Exercises
+
+Some exercises using Express, React and other technologies. The idea is to understand and implement some codes to practice
--- exercise 05/src/routes/transaction.routes.ts
+++ exercise 05/src/routes/transaction.routes.ts
@@ -9,7 +9,7 @@ const transactionRouter = Router();
 const transactionsRepository = new TransactionsRepository();
 transactionRouter.get('/', (request, response) => {
   try {
-    const data = transactionsRepository.getBalance();
+    const data = transactionsRepository.all();
 
     response.json(data);
   } catch (err) {
@@ -23,9 +23,15 @@ transactionRouter.post('/', (request, response) => {
     const createTransaction = new CreateTransactionService(
       transactionsRepository,
     );
+    const {total} = transactionsRepository.getBalance();
 
     if (type !== 'income' && type !== 'outcome') {
       throw Error('This transaction is not correct.');
+    }
+
+
+    if (type === 'outcome' && total < value){
+      throw Error('This transaction is not valid because the value is higher than available')
     }
 
     const transaction = createTransaction.execute({
